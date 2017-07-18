694c159153c975f294aaec3c8ee810313408ceeb
sizhijian <sizhijian619@163.com>
2017-07-18 20:01:41 +0800

    上传后的图片进行压缩，删除原有图片

--- routes/upload.js
+++ routes/upload.js
@@ -1,92 +1,88 @@
 var express = require('express'),
-    router = express.Router(),
-    usersSchema = require('../models/users'),
-    mongoose = require('mongoose'),
-    dbUrl = require('../config/db.conf'),
-    multiparty = require('multiparty'),
-    util = require('util'),
-    fs = require('fs');
+  router = express.Router(),
+  usersSchema = require('../models/users'),
+  mongoose = require('mongoose'),
+  dbUrl = require('../config/db.conf'),
+  multiparty = require('multiparty'),
+  util = require('util'),
+  images = require('images'),
+  fs = require('fs');
+
+mongoose.connect(dbUrl.url, {
+  useMongoClient: true
+});
+var User = mongoose.model('users', usersSchema); //将模式编译到模型中model('集合名称',...)会变成全小写
 
 
-mongoose.connect(dbUrl.url, {useMongoClient:true});
-var User = mongoose.model('users',usersSchema);//将模式编译到模型中model('集合名称',...)会变成全小写
 /* GET users listing. */
-router.post('/', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    var returnInfo = {
-	    state: 0,
-	    info:""
-    };
-    var form = new multiparty.Form();//生成multiparty对象，并配置上传目标路径
-    form.on('error', function(err) {
-      console.log('Error parsing form: ' + err.stack);
-      return;
-    });
-    form.uploadDir = __dirname + "./../public/files/";//设置文件存储路径
-    //上传完成后处理
-    form.parse(req, function (err, fields, files) {
-      console.log("1" + files.file[0].path);
-      res.send(returnInfo);
-      return;
-  //{ file:
-  //  [ { fieldName: 'file',
-  //      originalFilename: '微信图片_20170713183621.png',
-  //      path: 'D:\\shop-api\\public\\files\\MB6EDetCY2WaFIqO4iiPGT0R.png',
-  //      headers: [Object],
-  //      size: 1348825 } ] }
-      res.send(returnInfo);
-      return;
-        console.log(files.file[0].path);
-
-        res.send(returnInfo);
-        return;
-        var filesTmp = JSON.stringify(files, null, 2);
-            var username = fields.username[0];
-
-        if (err) {
-            console.log('parse error: ' + err);
-        } else {
-            var inputFile = files['file'][0];
-            //重命名为真实文件名
-            fs.rename(
-              inputFile.path,
-              form.uploadDir + "/"+ inputFile.originalFilename,
-              function (err) {
-                if (err) {
-                    console.log('rename error: ' + err);
-                } else {
-                    console.log('rename ok');
-                    console.log(username)
-                    User.findOne({username: username}, function(err, doc){
-                      if(err){
-                        console.log(err);
-                        return;
-                      }
-                      if (doc) {
-                        doc.avatarUrl = 'http://192.168.1.91:3000/files/' + inputFile.originalFilename;
-                        // doc.avatarUrl = 'http://sizhijian.com:3000/files/' + inputFile.originalFilename;
-                        doc.save(function(err){
-                          if (err) {
-                            console.log(err)
-                            returnInfo.info = '头像上传失败';
-                            res.send(returnInfo)
-                            return;
-                          }
-                          console.log('头像上传成功')
-                          returnInfo.state = 1;
-                          returnInfo.info = '头像上传成功';
-                          res.send(returnInfo)
-                          return;
-                        })
-                      }
-                    })
-                }
+router.post('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  var returnInfo = {
+    state: 0,
+    info: ""
+  };
+  var form = new multiparty.Form(); //生成multiparty对象，并配置上传目标路径
+  form.on('error', function(err) {
+    console.log('Error parsing form: ' + err.stack);
+    return;
+  });
+  form.uploadDir = __dirname + "./../public/files/"; //设置文件存储路径
+  //上传完成后处理
+  form.parse(req, function(err, fields, files) {
+    if (err) {
+      console.log('parse error: ' + err);
+    } else {
+      var inputFile = files['file'][0];
+      // console.log(files.file[0].path);
+      var filesTmp = JSON.stringify(files, null, 2);
+      var username = fields.username[0];
+      let imagesObj = images(files.file[0].path);
+      let imagesSize = 300;
+      if (imagesObj.width() > imagesObj.height() && imagesObj.width() > imagesSize) {
+        imagesObj.resize(imagesSize, null);
+      } else if (imagesObj.width() < imagesObj.height() && imagesObj.height() > imagesSize) {
+        imagesObj.width(imagesSize * (imagesObj.width() / imagesObj.height()))
+          .height(imagesSize);
+      }
+      imagesObj
+        .saveAsync(__dirname + "/../public/files/" + files['file'][0].originalFilename,
+          function() {
+            console.log('rename ok');
+            console.log(username);
+            fs.unlink(files.file[0].path,function(){
+              console.log("源文件删除成功")
+            })
+            User.findOne({
+              username: username
+            }, function(err, doc) {
+              if (err) {
+                console.log(err);
+                return;
+              }
+              if (doc) {
+                // doc.avatarUrl = 'http://192.168.1.91:3000/files/' + inputFile.originalFilename;
+                doc.avatarUrl = 'http://sizhijian.com:3000/files/' + inputFile.originalFilename;
+                doc.save(function(err) {
+                  if (err) {
+                    console.log(err)
+                    returnInfo.info = '头像上传失败';
+                    res.send(returnInfo)
+                    return;
+                  }
+                  console.log('头像上传成功')
+                  returnInfo.state = 1;
+                  returnInfo.info = '头像上传成功';
+                  res.send(returnInfo)
+                  return;
+                })
+              }
             });
-        }
-        // res.writeHead(200, {'content-type': 'text/plain;charset=utf-8'});
-        // res.write('received upload:\n\n');
-        // res.end(util.inspect({fields: fields, files: filesTmp}));
-    });
+          });
+    }
+    // res.writeHead(200, {'content-type': 'text/plain;charset=utf-8'});
+    // res.write('received upload:\n\n');
+    // res.end(util.inspect({fields: fields, files: filesTmp}));
+  });
 });
 
 module.exports = router;
